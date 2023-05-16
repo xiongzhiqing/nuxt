37aa5a8c9685e3eec6b5856b2a7f534c65937aee
熊志清 <xiongzhiqing@everjiankang.com>
2023-05-16 10:47:08 +0800

    feat: naive-ui

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,8 @@
 <template>
   <h1>Index Page</h1>
+  <div>
+    <NButton>hello</NButton>
+  </div>
   <NuxtLink to="/detail">detail</NuxtLink> |
   <NuxtLink to="/detail/1">detail-1</NuxtLink>
 </template>
